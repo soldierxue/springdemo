7b0b7a27d3a25e3546a789f5cf03be0fd941017f
soldierxue <sys@soldierxue.cn>
2017-04-28 14:32:27 +0800

    service update

--- spring-hw-1/src/main/java/springboot/App.java
+++ spring-hw-1/src/main/java/springboot/App.java
@@ -1,5 +1,6 @@
 package springboot;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Spring boot demo: Non Web Application Hello World!
  *
@@ -10,7 +11,6 @@ public class App
     public static void main( String[] args )
     {
         //System.out.println( "Hello World!" );
-        SpringApplication app = new SpringApplication(App.class);
-        app.run(args);
+        SpringApplication.run(App.class,args);
     }
 }
