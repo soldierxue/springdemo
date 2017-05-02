fea5b03915a325f1cd2b1b601d9d8dba27f8d000
soldierxue <sys@soldierxue.cn>
2017-05-02 10:01:00 +0800

    Spring Lambda Init

--- spring-hw-lambda/src/main/java/springboot/lambda/RequestComponent.java
+++ spring-hw-lambda/src/main/java/springboot/lambda/RequestComponent.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import springboot.lambda.*;
+import com.amazonaws.services.lambda.runtime.Context;
 /**
  * 
  */
